3eee7473918ded369b9ef72aaacf75bb20f429b2
LL <ll@ll.com>
2026-02-19 12:09:26 -0500

    Minor

--- func/deep_copy.lua
+++ func/deep_copy.lua
@@ -1,6 +1,7 @@
+local copy = {}
+
 function deep_copy(original)
     if type(original) ~= 'table' then return original end
-    local copy = {}
     for key, value in pairs(original) do copy[key] = deep_copy(value) end
     return copy
 end

3d5d1c0992b16dce54fc975cf751c6cc7854f548
LL <ll@ll.com>
2026-02-19 12:09:36 -0500

    Minor

--- func/current_scene_id.lua
+++ func/current_scene_id.lua
@@ -2,8 +2,6 @@
 -- Returns a app.constant.scn.Index.
 -- If there is no app.bFlowManager, it returns -1. I believe this only happens during the initial boot process.
 
-local sdk = sdk
-
 local function current_scene_id()
     local bFlowManager = sdk.get_managed_singleton("app.bFlowManager")
     if not bFlowManager then return -1 end

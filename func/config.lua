3a74c7f915f1b16cfd1bd58ecd72fb6a36d73589
LL <ll@ll.com>
2026-02-24 03:37:29 -0500

    Rework

--- func/config.lua
+++ func/config.lua
@@ -1,24 +1,32 @@
--- Config file handler with nested table support and custom ticker notifications
+-- Config file handler with nested table support and delayed save
 -- Usage: local Config, settings = config(path, settings, init_load)
 --   path: path to config file - default is (script_name).json
 --   settings: table of settings
 --   init_load: load settings on init, default true
 --
+-- Save behavior:
+--   Changes are automatically marked for save with a 0.5 second delay
+--   Multiple changes within the delay period are batched into a single save
+--   Saves are automatically flushed on script reset and config save events
+--
 -- Methods:
--- Config.add(key, val, table_name, show_ticker) Add single setting (top level table default)
--- Config.add(table, table_name, show_ticker) Add multiple settings
--- Config.remove(key, table_name, show_ticker) Remove single setting
--- Config.remove(table, table_name, show_ticker) Remove multiple settings
--- Config.save(show_ticker) Manually force immediate save
--- Config.load_all() Load all settings from file
--- Config.load(key, table_name) Load single setting
--- Config.get_table(table_name) Get reference to nested table
+--   Config.add(key, val, table_name) - Add single setting (top level table default)
+--   Config.add(table, table_name) - Add multiple settings
+--   Config.remove(key, table_name) - Remove single setting
+--   Config.remove(table, table_name) - Remove multiple settings
+--   Config.save() - Mark config for delayed save (0.5s)
+--   Config.save_now() - Force immediate save, bypassing delay
+--   Config.load_all() - Load all settings from file
+--   Config.load(key, table_name) - Load single setting from file
+--   Config.get_table(table_name) - Get reference to nested table
+--
+-- Example:
+--   local Config, settings = require("config")("my_mod.json", {foo = "bar"})
+--   Config.add("new_key", "value")  -- Marks for save with 0.5s delay
+--   Config.save_now()                 -- Saves immediately
 
-local json = json
-local fs = fs
-local re = re
-
-local ticker_func = require("func/show_custom_ticker")
+local SAVE_DELAY = 0.5
+local json, fs, re = json, fs, re
 
 local function get_default_path()
     local traceback = debug.traceback(nil, 3)
@@ -39,59 +47,35 @@ local function config(path, settings, init_load)
     local self = {
         path = path or get_default_path(),
         settings = settings or {},
-        dirty = false,
-        frame_counter = 0,
-        last_save_frame = 0,
-        save_throttle_frames = 300,
+        save_pending = false,
+        save_timer = 0,
         changed_keys = {},
     }
 
-    function self.show_save_ticker(changed_count)
-        local message = string.format(
-            "Config saved: %d setting%s changed",
-            changed_count,
-            changed_count == 1 and "" or "s"
-        )
-        
-        ticker_func(message, 0.4, 8)
-    end
-
-    function self.save_now(show_ticker)
+    function self.save_now()
         if not self.path or is_empty(self.settings) then return false end
         
         local result = json.dump_file(self.path, self.settings)
         if result then 
-            self.dirty = false
-            self.last_save_frame = self.frame_counter
-            
-            local changed_count = 0
-            for _ in pairs(self.changed_keys) do
-                changed_count = changed_count + 1
-            end
-            
-            if show_ticker and changed_count > 0 then
-                self.show_save_ticker(changed_count)
-            end
-            
+            self.save_pending = false
             self.changed_keys = {}
         end
         return result
     end
 
-    function self.save(show_ticker)
-        if not self.dirty then return false end
-        
-        local frames_since_last_save = self.frame_counter - self.last_save_frame
-        
-        if frames_since_last_save >= self.save_throttle_frames then
-            return self.save_now(show_ticker)
-        end
-        
-        if show_ticker then
-            self.pending_ticker = true
-        end
-        
+    function self.save()
+        self.save_pending = true
+        self.save_timer = SAVE_DELAY
         return true
+    end
+
+    function self.save_handler()
+        if self.save_pending then
+            self.save_timer = self.save_timer - (1.0 / 60.0)
+            if self.save_timer <= 0 then
+                self.save_now()
+            end
+        end
     end
 
     function self.get_table(table_name)
@@ -117,7 +101,6 @@ local function config(path, settings, init_load)
         
         if target[k] ~= v then
             target[k] = v
-            self.dirty = true
             
             local key_identifier = table_name and (table_name .. "." .. k) or k
             self.changed_keys[key_identifier] = true
@@ -138,23 +121,20 @@ local function config(path, settings, init_load)
         return changed
     end
 
-    function self.add(k_or_t, v_or_table, table_name_or_ticker, show_ticker)
+    function self.add(k_or_t, v_or_table, table_name)
         if not k_or_t then return false end
         local changed = false
         local actual_table_name = nil
-        local actual_show_ticker = false
         
         if type(k_or_t) == 'table' then
             actual_table_name = type(v_or_table) == 'string' and v_or_table or nil
-            actual_show_ticker = type(table_name_or_ticker) == 'boolean' and table_name_or_ticker or false
             changed = self.add_many(k_or_t, actual_table_name)
         elseif type(k_or_t) == 'string' and v_or_table ~= nil then
-            actual_table_name = type(table_name_or_ticker) == 'string' and table_name_or_ticker or nil
-            actual_show_ticker = type(show_ticker) == 'boolean' and show_ticker or false
+            actual_table_name = type(table_name) == 'string' and table_name or nil
             changed = self.add_one(k_or_t, v_or_table, actual_table_name)
         end
         
-        if changed then return self.save(actual_show_ticker) end 
+        if changed then return self.save() end 
         return changed
     end
 
@@ -164,7 +144,6 @@ local function config(path, settings, init_load)
         if not k or not target or target[k] == nil then return false end
         
         target[k] = nil
-        self.dirty = true
         
         local key_identifier = table_name and (table_name .. "." .. k) or k
         self.changed_keys[key_identifier] = true
@@ -183,25 +162,22 @@ local function config(path, settings, init_load)
         return changed
     end
 
-    function self.remove(k_or_t, table_name_or_ticker, show_ticker)
+    function self.remove(k_or_t, table_name)
         if not k_or_t then return false end
         
         local changed = false
         local actual_table_name = nil
-        local actual_show_ticker = false
         
         if type(k_or_t) == 'table' then
-            actual_table_name = type(table_name_or_ticker) == 'string' and table_name_or_ticker or nil
-            actual_show_ticker = type(show_ticker) == 'boolean' and show_ticker or false
+            actual_table_name = type(table_name) == 'string' and table_name or nil
             changed = self.remove_many(k_or_t, actual_table_name)
         elseif type(k_or_t) == 'string' then
-            actual_table_name = type(table_name_or_ticker) == 'string' and table_name_or_ticker or nil
-            actual_show_ticker = type(show_ticker) == 'boolean' and show_ticker or false
+            actual_table_name = type(table_name) == 'string' and table_name or nil
             changed = self.remove_one(k_or_t, actual_table_name)
         end
         
         if changed then
-            return self.save(actual_show_ticker)
+            return self.save()
         end
         return changed
     end
@@ -227,18 +203,15 @@ local function config(path, settings, init_load)
     end
 
     if init_load ~= false then self.settings = self.load_all() or {} end
-    re.on_script_reset(function() if self.dirty then self.save_now(false) end end)
-    re.on_config_save(function() if self.dirty then self.save_now(false) end end)
-    re.on_frame(function()
-        self.frame_counter = self.frame_counter + 1
-        if self.dirty then
-            local frames_since_last_save = self.frame_counter - self.last_save_frame
-            if frames_since_last_save >= self.save_throttle_frames then
-                self.save_now(self.pending_ticker or false)
-                self.pending_ticker = false
-            end
-        end
-    end)
+
+    -- Automatically create the config file if it doesn't exist and the path is valid
+    if self.path and not file_exists(self.path) then
+        self.save_now()
+    end
+
+    re.on_script_reset(function() if self.save_pending then self.save_now() end end)
+    re.on_config_save(function() if self.save_pending then self.save_now() end end)
+    re.on_frame(function() self.save_handler() end)
     
     return self, self.settings
 end
